93aa4fdf74cb14dcd86b42d4fc34b25a6ff81ff4
Marlon <marlonr@hey.com>
2023-09-25 20:52:46 -0400

    Basic view setup

--- WeatherPal/WeatherPal/Features/Weather Search/WeatherSearchViewController.swift
+++ WeatherPal/WeatherPal/Features/Weather Search/WeatherSearchViewController.swift
@@ -12,11 +12,46 @@ class WeatherSearchViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor = .systemPink
+		setupNavigationBar()
+		setupView()
 	}
 	
 	// MARK: - View Properties
 	
+	private lazy var searchController: UISearchController = {
+		let searchController = UISearchController(searchResultsController: nil)
+		searchController.delegate = self
+		searchController.searchResultsUpdater = self
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.automaticallyShowsCancelButton = true
+		searchController.hidesNavigationBarDuringPresentation = true
+		searchController.searchBar.placeholder = "The city to search weather for"
+		
+		return searchController
+	}()
 	
+	// MARK: - Setup
+	
+	private func setupNavigationBar() {
+		title = "☀️ Weather Pal"
+		
+		navigationItem.searchController = searchController
+		navigationController?.navigationItem.hidesSearchBarWhenScrolling = false
+		navigationController?.navigationBar.prefersLargeTitles = true
+	}
+	
+	private func setupView() {
+		view.backgroundColor = .systemBackground
+	}
 }
 
+// MARK: - Extensions
+
+extension WeatherSearchViewController: UISearchResultsUpdating {
+	
+	func updateSearchResults(for searchController: UISearchController) {
+		print(searchController.isActive)
+	}
+}
+
+extension WeatherSearchViewController: UISearchControllerDelegate {}
