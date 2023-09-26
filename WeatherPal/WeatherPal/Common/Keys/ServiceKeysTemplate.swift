686a69783c9d3a36f6bf6199247d226172977a97
Marlon <marlonr@hey.com>
2023-09-25 20:04:42 -0400

    Initial Commit

---
+++ WeatherPal/WeatherPal/Common/Keys/ServiceKeysTemplate.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceKeysTemplate.swift
+//  WeatherPal
+//
+//  Created by Marlon Raskin on 2023-09-25.
+//
+
+import Foundation
+
+// Remove the underscore below
+enum _APIKeys {
+	static let apiKey = "ADD_OPEN_WEATHER_API_KEY_HERE"
+}
---
+++ WeatherPal/WeatherPal/Features/Weather Search/WeatherSearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherSearchViewController.swift
+//  WeatherPal
+//
+//  Created by Marlon Raskin on 2023-09-25.
+//
+
+import UIKit
+
+class WeatherSearchViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .systemPink
+	}
+	
+	// MARK: - View Properties
+	
+	
+}
+
